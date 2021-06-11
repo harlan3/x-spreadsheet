09178694bcb5ca7bbd16864b36cc383807e7f7c8
Jared Kirschner <jared@jaredkirschner.com>
2021-06-11 09:52:49 -0400

    WIP: significant improvement to circular dependency detection and efficiency

--- src/core/cell.js
+++ src/core/cell.js
@@ -14,22 +14,23 @@ const isFormula = (src) => {
   return src && src.length > 0 && src[0] === '=';
 }
 
+// TODO: update this doc?
 // Whenever formulaParser.parser encounters a cell reference, it will
 // execute this callback to query the true value of that cell reference.
 // If the referenced cell contains a formula, we need to use formulaParser
 // to determine its value---which will then trigger more callCellValue
 // events to computer the values of its cell references. This recursion
 // will continue until the original formula is fully resolved.
-const getFormulaParserCellValueFromCoord = function(cellCoord) {
+const getCachedCellValueFromCoord = function(cellCoord) {
   const cell = cellGetOrNewFunction(cellCoord.row.index, cellCoord.column.index);
 
-  if (!cell) return '';
+  cellStack.push(cell);
 
-  return cell._recalculateCellValueFromText(cell.getText());
+  return cell.getValue();
 }
 
 formulaParser.on('callCellValue', function(cellCoord, done) {
-  const cellValue = getFormulaParserCellValueFromCoord(cellCoord);
+  const cellValue = getCachedCellValueFromCoord(cellCoord);
   done(cellValue);
 });
 
@@ -46,7 +47,7 @@ formulaParser.on('callRangeValue', function (startCellCoord, endCellCoord, done)
         row: { index: row },
         column: { index: col }
       };
-      const cellValue = getFormulaParserCellValueFromCoord(constructedCellCoord);
+      const cellValue = getCachedCellValueFromCoord(constructedCellCoord);
 
       colFragment.push(cellValue);
     }
@@ -60,8 +61,7 @@ class Cell {
   constructor(ri, ci, properties) {
     this.ri = ri;
     this.ci = ci;
-    this.updated = true;
-    this.uses = [];
+    this.uses = new Set();
     this.usedBy = new Map();
 
     // State contains what can be saved/restored
@@ -88,9 +88,8 @@ class Cell {
       return;
 
     this.state.text = text;
-    this.updated = false;
 
-    this.calculateValueFromText();
+    this.updateValueFromText();
   }
 
   set(fieldInfo, what = 'all') {
@@ -130,8 +129,6 @@ class Cell {
     if (what === 'text' || deleteAll) {
       // if (this.state.text) delete this.state.text;
       this.setText(undefined);
-
-      this.calculateValueFromText();
     }
     if (what === 'format' || deleteAll) {
       if (this.state.style !== undefined) delete this.state.style;
@@ -153,18 +150,122 @@ class Cell {
   }
 
   getValue() {
-    if (isFormula(this.state.text))
-      return this.value;
+    return this.value;
+    // if (isFormula(this.state.text))
+    //   return this.value;
 
-    return this.getText();
+    // return this.getText();
   }
 
-  calculateValueFromText() {
+  updateValueFromTextInternal() {
+    let src = this.state.text;
+
+    if (isFormula(src)) {
+      // All dependent cells referenced are added to cellStack by the
+      // callCellValue and callRangeValue event handlers
+      const parsedResult = formulaParser.parse(src.slice(1));
+      src = (parsedResult.error) ?
+                parsedResult.error :
+                parsedResult.result;
+    }
+
+    // The source string no longer contains a formula,
+    // so return its contents as a value.
+    // Else if said string is a number, return as a number;
+    // otherwise, return as a string.
+    // Else (e.g., src is undefined), return an empty string.
+    this.value = Number(src) || src || '';
+  }
+
+  updateValueFromText() {
     cellStack = [];
 
-    // resetDependencies = true;
-    this._recalculateCellValueFromText();
-    // resetDependencies = false;
+    this.updateValueFromTextInternal();
+
+    // Copy of existing array of cells used by this formula;
+    // will be used to see how dependencies have changed.
+    let oldUses = new Set(this.uses);
+    this.uses = new Set(cellStack);
+
+    // ------------------------------------------------------------------------
+    // Update cell reference dependencies
+
+    // Build temporary weakmaps from the previous and current arrays of cells
+    // used by this cell's formula for faster determination of how those
+    // dependencies have changed (than comparing two arrays).
+    const oldUsesWeakMap = new WeakMap();
+    oldUses.forEach((cell) => oldUsesWeakMap.set(cell, true));
+
+    const usesWeakMap = new WeakMap();
+    this.uses.forEach((cell) => usesWeakMap.set(cell, true));
+
+    // Cells that this cell's formula previously used, but no longer does
+    const noLongerUses = Array.from(oldUses).filter((cell) => !usesWeakMap.has(cell));
+
+    // Notify cells no longer in use that this cell no longer depends on
+    // them, and therefore doesn't need to be forced to update when they do.
+    noLongerUses.forEach((cell) => cell.noLongerUsedByCell(this));
+
+    // Cells that this cell's formula didn't previously use, but now does
+    const nowUses = Array.from(this.uses).filter((cell) => !oldUsesWeakMap.has(cell));
+
+    // Notify cells now in use that this cell needs to be forced to update
+    // when they do.
+    nowUses.forEach((cell) => cell.usedByCell(this));
+
+    // ------------------------------------------------------------------------
+    // Trigger update of dependent cells and check for dependency graph cycles
+
+    const dfsStack = [];
+    const visitedMap = new WeakMap();
+
+    const updateDependenciesWithCycleCheck = (cell) => {
+      // BUG/TODO: if a cell uses a circular cell, its value also needs to be fixed
+
+      // Check for cycles:
+      // If this cell is already in the dfsStack, there is a cycle from that
+      // index to the end of the stack
+      const indexOfCycleStart = dfsStack.indexOf(cell);
+      if (indexOfCycleStart >= 0) {
+        console.log('circular!!!!!!!', indexOfCycleStart);
+        // TODO:
+        // Mark all cells from that point forward as cyclic
+        dfsStack.slice(indexOfCycleStart).forEach((cell) => {
+          cell.value = '#CIRCULAR';
+        });
+      }
+
+      // If this cell has been visited before, return early to avoid both
+      // unnecessary computation and a possible cycle when iterating through
+      // dependent cells.
+      if (visitedMap.has(cell)) return;
+
+      // Add to stack before recursion so dependent cells can include this cell
+      // in their cycle check
+      dfsStack.push(cell);
+      console.log('stack', dfsStack.map((cell) => cell.state.text));
+      visitedMap.set(cell, true);
+
+      // Iterate through all dependent cells,
+      // trigger them to update the values of themselves and their dependencies.
+      cell.usedBy.forEach((columnMap, ri) => {
+        columnMap.forEach((dependentCell, ci) => {
+          console.log('dep cell', dependentCell.state.text);
+          // Trigger the cell to update; because if is using cached cell values
+          // rather than recalculating them, we don't have to worry about
+          // causing infinite recursion in case of cycles.
+          dependentCell.updateValueFromTextInternal();
+
+          // Trigger the cell to update its own dependencies
+          updateDependenciesWithCycleCheck(dependentCell);
+        });
+      });
+
+      // Remove self from the stack
+      dfsStack.pop();
+    };
+
+    updateDependenciesWithCycleCheck(this);
   }
 
   usedByCell(cell) {
@@ -182,130 +283,6 @@ class Cell {
     // Delete Map for row if now empty
     if (this.usedBy.get(cell.ri).size == 0) this.usedBy.delete(cell.ri);
   }
-
-  _recalculateCellValueFromText() {
-    let src = this.state.text;
-
-    // Need to store here rather than later in the function in case calls to
-    // formulaParser.parse cause resetDependencies to be modified
-    // let originalResetDependenciesState = resetDependencies;
-
-    // Only necessary if dependencies are being reset.
-    // if (resetDependencies) {
-      cellStack.push(this);
-    // }
-
-    if (this.updated) return this.value;
-
-    // Copy of existing array of cells used by this formula;
-    // will be used to see how dependencies have changed.
-    let oldUses = this.uses.slice();
-    this.uses = [];
-
-    if (isFormula(src)) {
-      const parsedResult = formulaParser.parse(src.slice(1));
-      src = (parsedResult.error) ?
-                parsedResult.error :
-                parsedResult.result;
-
-      // if (resetDependencies) {
-        // Store new dependencies of this cell by popping cells off the cell stack
-        // until this cell is reached.
-        while (this !== cellStack[cellStack.length - 1]) {
-          this.uses.push(cellStack.pop());
-        }
-
-        const recursiveCircularDependencyCheck = (cellToCheckDependencies) => {
-          // A circular dependency is detected if either:
-          // - cellToCheckDependencies uses this cell
-          // - a cell used (recursively) by cellToCheckDependencies uses this cell
-          return cellToCheckDependencies.uses.some((cell) => {
-            if (cell === this) return true;
-
-            return recursiveCircularDependencyCheck(cell);
-          });
-        };
-
-        this.circular = recursiveCircularDependencyCheck(this);
-        if (this.circular) {
-          src = '#CIRCULAR';
-        }
-      // }
-    } else {
-      // Non-formulas can't be circular
-      this.circular = false;
-    }
-
-    // The source string no longer contains a formula,
-    // so return its contents as a value.
-    // Else if said string is a number, return as a number;
-    // otherwise, return as a string.
-    // Else (e.g., src is undefined), return an empty string.
-    this.value = Number(src) || src || '';
-    this.updated = true;
-
-    // ------------------------------------------------------------------------
-    // Update cell reference dependencies and trigger update of dependent cells
-
-    // if (resetDependencies) {
-      // Build temporary weakmaps from the previous and current arrays of cells
-      // used by this cell's formula for faster determination of how those
-      // dependencies have changed (than comparing two arrays).
-      const oldUsesWeakMap = new WeakMap();
-      oldUses.forEach((cell) => oldUsesWeakMap.set(cell, true));
-
-      const usesWeakMap = new WeakMap();
-      this.uses.forEach((cell) => usesWeakMap.set(cell, true));
-
-      // Cells that this cell's formula previously used, but no longer does
-      const noLongerUses = oldUses.filter((cell) => !usesWeakMap.has(cell));
-
-      // Notify cells no longer in use that this cell no longer depends on
-      // them, and therefore doesn't need to be forced to update when they do.
-      noLongerUses.forEach((cell) => cell.noLongerUsedByCell(this));
-
-      // Cells that this cell's formula didn't previously use, but now does
-      const nowUses = this.uses.filter((cell) => !oldUsesWeakMap.has(cell));
-
-      // Notify cells now in use that this cell needs to be forced to update
-      // when they do.
-      nowUses.forEach((cell) => cell.usedByCell(this));
-    // }
-
-    // ------------------------------------------------------------------------
-    // Iterate through this cell's registry of cells that use it and force them
-    // to update their value, but change no dependencies.
-
-    // Dependencies should not be updated in these calls. This also keeps the
-    // cellStack unmodified by triggered updates.
-    // let originalResetDependenciesState = resetDependencies;
-    // resetDependencies = false;
-
-    this.usedBy.forEach((columnMap, ri) => {
-      columnMap.forEach((cell, ci) => {
-        // If this cell is circular AND the next cell is circular, skip it to
-        // avoid infinite recursion.
-        // If the user edits a previously circular cell such that it is no
-        // longer circular (this.circular == false), recompute the cells it is
-        // used by to see if their circularity has been resolved.
-        if (this.circular && cell.circular) return;
-
-        // Force update
-        cell.updated = false;
-        cell._recalculateCellValueFromText();
-      });
-    });
-
-    // Restore original resetDependencies state.
-    // For cells in this.usedBy forced to recalculate, resetDependencies will
-    // restore to false ensuring that nothing in this.usedBy recalculates its
-    // dependencies.
-    // For cells parsed as a result of a calculateValueFromText call, this will
-    // restore to true ensuring that dependencies are updated.
-    // resetDependencies = originalResetDependenciesState;
-
-    return this.value;
-  };
 
   getStateCopy() {
     return helper.cloneDeep(this.state);
